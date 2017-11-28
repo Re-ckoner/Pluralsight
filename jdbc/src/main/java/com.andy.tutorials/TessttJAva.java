68d92e2379e19e7f19d22218b2b00b49bf3708ed
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-11-29 01:47:47 +0300

    helloworld

---
+++ jdbc/src/main/java/com.andy.tutorials/TessttJAva.java
@@ -0,0 +1,11 @@
+package com.andy.tutorials;
+
+/**
+ * Created by Andy on 29.11.2017.
+ */
+public class TessttJAva {
+    public static void main(String[] args) {
+        System.out.print("JDBC stuff");
+
+    }
+}
